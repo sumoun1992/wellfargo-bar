82ee689fa4adc6ff71799f77a3a6cb10dd1b72f7
sumoun1992 <51076832+sumoun1992@users.noreply.github.com>
2019-05-28 08:46:09 +0530

    Update sumoun1.sh

--- sumoun1.sh
+++ sumoun1.sh
@@ -2,7 +2,7 @@
 
 pwd
 
-echo "my name is bittu and environment choice is $ENV" >> $filename
+echo "my name is bittu and environment choices is $ENV" >> $filename
 /bin/ls -ltr
 a=`/bin/cat $filename`
 /bin/ls -ltr
